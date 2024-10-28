c30027115ee6915585e39a84f4522d0a262c1fed
Sahil Jain <sahiljai@myth56.stanford.edu>
2024-10-28 15:36:16 -0700

    m

--- part_a/tasksys.cpp
+++ part_a/tasksys.cpp
@@ -248,7 +248,9 @@ void TaskSystemParallelThreadPoolSleeping::run(IRunnable* runnable, int num_tota
     m_tss->m_queueSize = num_total_tasks;
     m_tss->m_completedCount.store(0);
     lk.unlock();
-    m_tss->m_notifyWorkersCV->notify_all();
+    while (m_tss->m_waitingThreads != m_numThreads) {
+	    m_tss->m_notifyWorkersCV->notify_all();
+    }
     m_tss->m_notifySignalCV->wait(finishedLk);
     finishedLk.unlock();
 }
