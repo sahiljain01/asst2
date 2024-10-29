bf6be325e88b9367c19387f9bf5f1abf33293ee6
Sahil Jain <sahiljai@myth57.stanford.edu>
2024-10-28 18:58:12 -0700

    m

--- part_a/tasksys.cpp
+++ part_a/tasksys.cpp
@@ -187,7 +187,6 @@ void spawnThreadSleeping(TaskSystemStateCV* ts, int threadId) {
 				std::unique_lock<std::mutex> finishedLk(*ts->m_finishedMutex);
 				finishedLk.unlock();
 				ts->m_notifySignalCV->notify_all();
-				brokeOutOfLoop = true;
 				break;
 			}
 			lk.lock();
@@ -198,7 +197,6 @@ void spawnThreadSleeping(TaskSystemStateCV* ts, int threadId) {
 		//	lk.unlock();
 		// }
 	        ts->m_notifyWorkersCV->wait(lk);
-	        lk.unlock();	
 	}
 }
 
